ce7f5284fe90ab93dd3377338752aa35cf5975a8
Николай Глущенко <clausstolz@yandex.ru>
2018-12-01 10:06:14 +1000

    Init generator engine class

---
+++ SQRA-generator/SQRA-generator/EngineSQRA.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SQRA_generator
+{
+    public class EngineSQRA
+    {
+        public string Data { get; set; }
+
+        public EngineSQRA(string aData)
+        {
+            this.Data = aData;
+        }
+    }
+}
