8ee9b8ac2e3ae8e77740de1341aa297585bfe6a5
Богдан Цечоев <flomaster.comix@gmail.com>
2018-12-01 19:32:48 +1000

    add web generator

--- Test Server/Test Server/Controllers/HomeController.cs
+++ Test Server/Test Server/Controllers/HomeController.cs
@@ -5,11 +5,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Test_Server.Models;
+using SQRA_generator;
 
 namespace Test_Server.Controllers
 {
     public class HomeController : Controller
     {
+
+        private static string textData;
         public IActionResult Index()
         {
             //Dictionary<int, bool> testsDict = new Dictionary<int, bool>();
@@ -35,6 +38,28 @@ namespace Test_Server.Controllers
 
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Generate(string text)
+        {
+            //ViewBag.Text = text;
+            textData = text;
+            EngineSQRA webSQRA = new EngineSQRA(80, 50, textData);
+            webSQRA.GenerateQRA();
+            
+            return View();
+        }
+
+       
+
+
+        public IActionResult Generate()
+        {
+            
+
+            return View();
+        }
+
 
         public IActionResult Error()
         {
---
+++ Test Server/Test Server/EngineSQRA.cs
@@ -0,0 +1,92 @@
+﻿
+using System.Drawing;
+using System.Drawing.Imaging;
+using QRCoder;
+using System.IO;
+using ImageMagick;
+using System.Collections.Generic;
+
+namespace SQRA_generator
+{
+    public class EngineSQRA
+    {
+        private List<string> data;
+        private int elementLength;
+        private int animationDelay;
+
+        public EngineSQRA(int aElementLength, int aAnimationDelay, string aData) 
+        {
+            data = new List<string>();
+
+            elementLength  = aElementLength;
+            animationDelay = aAnimationDelay;
+
+            while (aData.Length > elementLength)
+            {
+                var element = aData.Substring(0, elementLength);
+                aData = aData.Remove(0, elementLength);
+                data.Add(element);
+            }
+
+            if(aData.Length != 0)
+            {
+                data.Add(aData);
+            }
+        }
+
+        private void GeneratePngFileFromData(int aPartIndex, int aCount, string aPartData)
+        {
+            string sumData = aPartIndex.ToString() 
+                           + "|" 
+                           + aCount.ToString() 
+                           + "|" 
+                           + aPartData;
+
+
+            QRCodeGenerator qrCodeGenerator = new QRCodeGenerator();
+            QRCodeData           qrCodeData = qrCodeGenerator.CreateQrCode(sumData, QRCodeGenerator.ECCLevel.M);
+            PngByteQRCode            qrCode = new PngByteQRCode(qrCodeData);
+
+            byte[] qrCodeAsPngByteArray = qrCode.GetGraphic(20);
+
+            using (Image image = Image.FromStream(new MemoryStream(qrCodeAsPngByteArray)))
+            {
+                image.Save("test" + aPartIndex.ToString() + ".png", ImageFormat.Png);
+            }
+        }
+
+        private void GenerateGif(int aCount, int aAnimationDelay, string aGifName)
+        {
+            using (MagickImageCollection collection = new MagickImageCollection())
+            {
+                for (int i = 0; i < aCount; i++)
+                {
+                    collection.Add("test" + i.ToString() + ".png");
+                    collection[i].AnimationDelay = aAnimationDelay;
+                }
+                foreach (MagickImage image in collection)
+                {
+                    image.Resize(250, 0);
+                }
+                QuantizeSettings settings = new QuantizeSettings
+                {
+                    Colors = 256
+                };
+
+                collection.Quantize(settings);
+                collection.Optimize();
+
+                collection.Write(aGifName + ".gif");
+            }
+        }
+
+        public void GenerateQRA()
+        {
+            for (int i = 0; i < data.Count; i++)
+            {
+                GeneratePngFileFromData(i, data.Count, data[i]);
+            }
+            GenerateGif(data.Count, animationDelay, "test");
+        }
+    }
+}
