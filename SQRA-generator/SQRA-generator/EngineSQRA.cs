8d92e55c2bf82cae066e3c6efee82186253ab8fa
Николай Глущенко <clausstolz@yandex.ru>
2018-12-01 11:06:24 +1000

    Add generation simple qr code from data

--- SQRA-generator/SQRA-generator/EngineSQRA.cs
+++ SQRA-generator/SQRA-generator/EngineSQRA.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using QRCoder;
+using System.IO;
+
 namespace SQRA_generator
 {
     public class EngineSQRA
@@ -8,6 +13,16 @@ namespace SQRA_generator
         public EngineSQRA(string aData)
         {
             this.Data = aData;
+        }
+
+        public void GenerateFromData(string aData)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode("The text which should be encoded.", QRCodeGenerator.ECCLevel.Q);
+            SvgQRCode qrCode = new SvgQRCode(qrCodeData);
+            string qrCodeAsSvg = qrCode.GetGraphic(20);
+
+            File.WriteAllText("test.svg", qrCodeAsSvg);
         }
     }
 }
