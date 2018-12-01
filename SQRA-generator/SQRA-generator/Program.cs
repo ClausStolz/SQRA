3777234453020b36dc5d81fb790567ed98ecaf1e
Николай Глущенко <clausstolz@yandex.ru>
2018-12-01 10:06:02 +1000

    Init generator project

---
+++ SQRA-generator/SQRA-generator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SQRA_generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
