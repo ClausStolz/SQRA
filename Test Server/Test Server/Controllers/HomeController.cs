d2baa1997a7ca13893eddb3ddf3c5437cd96b6d0
Богдан Цечоев <flomaster.comix@gmail.com>
2018-12-03 16:42:37 +1000

    add gihub link and gplay/app store badges

--- Test Server/Test Server/Controllers/HomeController.cs
+++ Test Server/Test Server/Controllers/HomeController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Mvc;
 using Test_Server.Models;
 using SQRA_generator;
+
 
 namespace Test_Server.Controllers
 {
@@ -15,42 +17,18 @@ namespace Test_Server.Controllers
         private static string textData;
         public IActionResult Index()
         {
-            //Dictionary<int, bool> testsDict = new Dictionary<int, bool>();
-            //testsDict.Add(1, true);
-            //testsDict.Add(2, false);
-            HomeModel HomeModel = new HomeModel();
-
-            //ViewBag.testsDict = testsDict;
-            ViewData["tests"] = HomeModel.ReturnDict().Count;
             return View(ViewData);
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
         }
 
         [HttpPost]
         public IActionResult Generate(string text)
         {
-            //ViewBag.Text = text;
-            textData = text;
-            EngineSQRA webSQRA = new EngineSQRA(80, 50, textData);
+            EngineSQRA webSQRA = new EngineSQRA(80, 50, text);
             webSQRA.GenerateQRA();
             
             return View();
         }
 
-       
 
 
         public IActionResult Generate()
